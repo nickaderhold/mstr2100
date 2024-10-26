3157997e092bcd201e98e969167ce27e4db9fb1f
Nick <aderholdnick43@gmail.com>
2024-10-26 11:44:53 -0400

    coming soon

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
-import React, { useRef } from 'react';
-import Header from './components/Header';
-import Subfooter from './components/Subfooter';
-import UniswapTrade from './components/UniswapTrade';
-import Chart from './components/Chart';
-import Videos from './components/Videos';
-import Footer from './components/Footer'; // Import Footer component
-import MergedComponent from './components/MergedComponent'; // Import the merged component
-import StockTicker from './components/StockTicker';
+import React from 'react';
+import ComingSoon from './components/ComingSoon'; // Import ComingSoon component
 
 function App() {
-  const uniswapRef = useRef(null);
-  const chartRef = useRef(null);
-
-  const handleBuyClick = () => {
-    if (uniswapRef.current) {
-      uniswapRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
-  const handleChartClick = () => {
-    if (chartRef.current) {
-      chartRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
   return (
     <div className="h-screen flex flex-col">
-      <Header />
+      {/* Commented out sections */}
+      {/* <Header />
       <Subfooter onBuyClick={handleBuyClick} onChartClick={handleChartClick} />
       <StockTicker />
-
-      {/* Replace LeftPanel, StorySection, and StockTicker with MergedComponent */}
       <div className="flex-grow flex flex-col lg:flex-row">
         <MergedComponent />
       </div>
-
       <div ref={uniswapRef}>
         <UniswapTrade />
       </div>
@@ -42,12 +18,16 @@ function App() {
         <Chart />
       </div>
       <Videos />
-      <Footer />
+      <Footer /> */}
+
+      {/* Replace everything with ComingSoon */}
+      <ComingSoon />
     </div>
   );
 }
 
 export default App;
+
 
 
 
